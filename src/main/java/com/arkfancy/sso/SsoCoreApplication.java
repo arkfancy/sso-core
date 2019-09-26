746b441945a2f38bacda2258652153604ae7729a
arkfancy <ark_light_0@163.com>
2019-09-26 15:01:53 +0800

    init

---
+++ src/main/java/com/arkfancy/sso/SsoCoreApplication.java
@@ -0,0 +1,13 @@
+package com.arkfancy.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoCoreApplication.class, args);
+	}
+
+}
